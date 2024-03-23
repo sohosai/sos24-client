d489c9c6b285f5a9c075c5f39e0b46566577459c
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-24 00:49:39 +0900

    assign the suitable type to responses

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,31 +3,40 @@
 import { fetcherWithToken } from "@/lib/swr";
 import { NextPage } from "next";
 import useSWR from "swr";
+import { assignType } from "@/lib/openapi";
 import { NewsList } from "./NewsList";
 import { css } from "@styled-system/css";
 
 const DashboardPage: NextPage = () => {
-  const news = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/news`, fetcherWithToken);
+  const { data: newsRes, error: newsErr } = useSWR("/news", fetcherWithToken);
+  console.log(newsErr);
+  const news = newsRes ? assignType("/news", newsRes) : undefined;
+
+  news?.map((n) => console.log(n));
 
   return (
     <div
       className={css({
         padding: 5,
       })}>
-      <NewsList
-        newsList={[
-          {
-            id: "ID",
-            title: "タイトル",
-            updated_at: new Date().toISOString(),
-          },
-          {
-            id: "ID",
-            title: "タイトル",
-            updated_at: new Date().toISOString(),
-          },
-        ]}
-      />
+      {newsErr ? (
+        <p>お知らせの取得中にエラーが発生しました({String(newsErr)})</p>
+      ) : (
+        <NewsList
+          newsList={[
+            {
+              id: "ID",
+              title: "タイトル",
+              updated_at: new Date().toISOString(),
+            },
+            {
+              id: "ID",
+              title: "タイトル",
+              updated_at: new Date().toISOString(),
+            },
+          ]}
+        />
+      )}
     </div>
   );
 };
