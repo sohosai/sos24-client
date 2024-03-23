0986812d71d1b7f058cb05af66d40d1ccfc6a7b5
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-24 01:35:01 +0900

    refactor

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -4,11 +4,7 @@ import type { PathsWithMethod } from "openapi-typescript-helpers";
 export const assignType = <P extends PathsWithMethod<paths, "get">>(path: P, response_json: any) => {
   type responses = paths[P]["get"]["responses"];
 
-  return response_json as responses extends { "200": {} }
-    ? responses["200"] extends { content: {} }
-      ? responses["200"]["content"] extends { "application/json": {} }
-        ? responses["200"]["content"]["application/json"]
-        : any
-      : any
+  return response_json as responses extends { "200": { content: { "application/json": {} } } }
+    ? responses["200"]["content"]["application/json"]
     : any;
 };
