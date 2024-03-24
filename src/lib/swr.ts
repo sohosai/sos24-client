80c187b9095aa93248c8241c1f77c06e609bbe3d
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-24 15:26:16 +0900

    remove gabages

--- src/lib/swr.ts
+++ src/lib/swr.ts
@@ -1,6 +1,6 @@
 export const fetcherWithToken = async (url: string, token?: string, init?: RequestInit) => {
   if (!token) {
-    //throw new Error("Unauthorized");
+    throw new Error("Unauthorized");
   }
 
   return await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}${url}`, {
