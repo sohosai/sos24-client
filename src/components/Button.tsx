b2c90f9ec6d4cbc8923e71bf498a591f8a80a69e
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-24 18:59:26 +0900

    add a blue button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { cva } from "@styled-system/css";
 import { FC } from "react";
 
 type Props = {
-  color: "primary" | "secondary";
+  color: "primary" | "secondary"| "blue";
   size: "medium" | "big";
   children: React.ReactNode;
 };
@@ -34,6 +34,18 @@ export const Button: FC<Props> = ({ color, size, children }: Props) => {
           borderStyle: "solid",
           borderColor: "primary",
         },
+        blue:{
+            backgroundColor: "white",
+            color: "sohosai.blue",
+            _hover: {
+              opacity: "75%",
+            },
+
+            borderWidth: 2,
+            borderStyle: "solid",
+            borderColor: "sohosai.blue",
+            boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+            }
       },
       size: {
         medium: { paddingInline: 3, paddingBlock: 1 },
