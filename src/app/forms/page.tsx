81a5268428aa38957da57c4b82b77e692c2b56d6
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-25 15:46:15 +0900

    better error handling

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -11,15 +11,15 @@ import { Button } from "@/components/Button";
 import { FormsList } from "./FormsList";
 
 const DashboardPage: NextPage = () => {
-  const { data: projectRes, error: projectErr } = useSWR("/projects/me", fetcherWithToken);
-  const project = projectRes ? assignType("/projects/me", projectRes) : undefined;
+  const { data: projectRes } = useSWR("/projects/me", fetcherWithToken);
+  const project = projectRes ? assignType("/projects/me", projectRes.json) : undefined;
 
   const projectId = project?.id;
-  const { data: formsRes, error: formsErr } = useSWR(`/forms?project_id=${projectId}`, fetcherWithToken);
-  const forms = formsRes ? assignType("/forms", formsRes) : undefined;
+  const { data: formsRes } = useSWR(`/forms?project_id=${projectId}`, fetcherWithToken);
+  const forms = formsRes ? assignType("/forms", formsRes.json) : undefined;
 
-  const { data: answersRes, error: answersErr } = useSWR(`/form-answers?project_id=${projectId}`, fetcherWithToken);
-  const answers = answersRes ? assignType("/form-answers", answersRes) : undefined;
+  const { data: answersRes } = useSWR(`/form-answers?project_id=${projectId}`, fetcherWithToken);
+  const answers = answersRes ? assignType("/form-answers", answersRes.json) : undefined;
 
   const notification = forms && answers ? forms.length - answers.length : 0;
 
@@ -54,22 +54,24 @@ const DashboardPage: NextPage = () => {
             maxWidth: "900px",
             marginInline: "auto",
           })}>
-          <Button
-            color={filterUnsubmitted ? "primary" : "secondary"}
-            onClick={() => setFilterUnsubmitted(!filterUnsubmitted)}
-            onTouchEnd={() => setFilterUnsubmitted(!filterUnsubmitted)}>
-            未提出のみ表示
-          </Button>
-          {projectErr || formsErr || answersErr ? (
+          {(projectRes && !projectRes.ok) || (formsRes && !formsRes?.ok) || (answersRes && !answersRes?.ok) ? (
             <p>
               フォームの取得中にエラーが発生しました(
-              {(projectErr ? `Project: ${projectErr}` : "") +
-                (formsErr ? `Forms: ${formsErr}` : "") +
-                (answersErr ? `Answers: ${answersErr}` : "")}
+              {(projectRes && !projectRes?.ok ? `Project: ${projectRes?.statusCode} ` : "") +
+                (formsRes && !formsRes?.ok ? `Forms: ${formsRes?.statusCode} ` : "") +
+                (answersRes && !answersRes?.ok ? `Answers: ${answersRes?.statusCode}` : "")}
               )
             </p>
           ) : (
-            <FormsList forms={forms} answers={answers} filterUnsubmitted={filterUnsubmitted} />
+            <>
+              <Button
+                color={filterUnsubmitted ? "primary" : "secondary"}
+                onClick={() => setFilterUnsubmitted(!filterUnsubmitted)}
+                onTouchEnd={() => setFilterUnsubmitted(!filterUnsubmitted)}>
+                未提出のみ表示
+              </Button>
+              <FormsList forms={forms} answers={answers} filterUnsubmitted={filterUnsubmitted} />
+            </>
           )}
         </div>
       </div>
