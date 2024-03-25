1aef921110601f196581cc0a6d4b9c5e60175d60
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-26 01:08:49 +0900

    implement required badge

---
+++ src/components/forms/RequiredBadge.tsx
@@ -0,0 +1,28 @@
+import { cva } from "@styled-system/css"
+
+export const RequiredBadge = ({isRequired}: {isRequired: boolean}) => {
+    const style = cva({
+        base: {
+            paddingBlock: 0.5, 
+            paddingInline: 3,
+            borderRadius: "sm",
+            color: "white",
+            fontWeight: "bold"
+        },
+        variants: {
+            isRequired: {
+                true: {
+                    backgroundColor: "black"
+                },
+                false: {
+                    backgroundColor: "gray.500"
+                }
+            }
+        }
+
+        })
+
+    return (
+    <span className={style({isRequired: isRequired})}>{isRequired ? "必須" : "任意"}</span>
+    )
+}
