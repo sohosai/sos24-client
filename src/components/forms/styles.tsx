0b7c4a7b821522cb687453ab4141ae0c0b8c56dd
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-26 16:19:40 +0900

    commonize form styles

---
+++ src/components/forms/styles.tsx
@@ -0,0 +1,12 @@
+import { css } from "@styled-system/css";
+
+export const basicFormStyle = css({
+  width: "100%",
+  backgroundColor: "gray.100",
+  padding: 2,
+
+  borderRadius: "md",
+  borderWidth: 2,
+  borderStyle: "solid",
+  borderColor: "gray.400",
+});
