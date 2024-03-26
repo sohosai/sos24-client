379a431f57f584d2a195de6395141beecb50d682
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-26 14:25:26 +0900

    format

--- src/components/forms/RequiredBadge.tsx
+++ src/components/forms/RequiredBadge.tsx
@@ -1,28 +1,25 @@
-import { cva } from "@styled-system/css"
+import { cva } from "@styled-system/css";
 
-export const RequiredBadge = ({isRequired}: {isRequired: boolean}) => {
-    const style = cva({
-        base: {
-            paddingBlock: 0.5, 
-            paddingInline: 3,
-            borderRadius: "sm",
-            color: "white",
-            fontWeight: "bold"
+export const RequiredBadge = ({ isRequired }: { isRequired: boolean }) => {
+  const style = cva({
+    base: {
+      paddingBlock: 0.5,
+      paddingInline: 3,
+      borderRadius: "sm",
+      color: "white",
+      fontWeight: "bold",
+    },
+    variants: {
+      isRequired: {
+        true: {
+          backgroundColor: "black",
         },
-        variants: {
-            isRequired: {
-                true: {
-                    backgroundColor: "black"
-                },
-                false: {
-                    backgroundColor: "gray.500"
-                }
-            }
-        }
+        false: {
+          backgroundColor: "gray.500",
+        },
+      },
+    },
+  });
 
-        })
-
-    return (
-    <span className={style({isRequired: isRequired})}>{isRequired ? "必須" : "任意"}</span>
-    )
-}
+  return <span className={style({ isRequired: isRequired })}>{isRequired ? "必須" : "任意"}</span>;
+};
