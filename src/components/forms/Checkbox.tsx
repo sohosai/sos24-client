9e3477a83f0fe9e7c4c67d8e54b1a256d601ae06
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-26 21:23:15 +0900

    fix checkbox

--- src/components/forms/Checkbox.tsx
+++ src/components/forms/Checkbox.tsx
@@ -45,6 +45,8 @@ export const CheckboxForm: FC<Props> = (props: Props) => {
                     borderRadius: "sm",
                     borderStyle: "solid",
                     borderColor: "gray.400",
+                    cursor: "pointer",
+
                     backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' width='18' height='12' viewBox='0 0 18 12'%3e%3cpath stroke='%23A59E9E' stroke-linecap='round' stroke-linejoin='round' stroke-width='2.5' d='M2 5.7L7.0541 10.5L16 2'/%3e%3c/svg%3e")`,
                     backgroundRepeat: "no-repeat",
                     backgroundPositionX: "center",
@@ -59,8 +61,10 @@ export const CheckboxForm: FC<Props> = (props: Props) => {
                 <label
                   htmlFor={`${props.id}-${index}`}
                   className={css({
-                    marginInline: 2,
+                    paddingInline: 2,
+                    width: "100%",
                     fontSize: 18,
+                    cursor: "pointer",
                   })}>
                   {option}
                 </label>
