dc3bef3b95da1d6a81c464150caf89529283a279
OKAMOTO Shigehiro <primary@goahi.live>
2024-04-01 23:58:59 +0900

    split out the style of checkbox

--- src/components/forms/styles.tsx
+++ src/components/forms/styles.tsx
@@ -36,3 +36,24 @@ export const basicErrorMessageStyle = css({
   color: "error",
   fontSize: "sm"
 });
+
+export const checkboxFormStyle = css({
+  appearance: "none",
+  height: 7,
+  width: 7,
+  border: 3,
+  borderRadius: "sm",
+  borderStyle: "solid",
+  borderColor: "gray.400",
+  cursor: "pointer",
+
+  backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' width='18' height='12' viewBox='0 0 18 12'%3e%3cpath stroke='%23A59E9E' stroke-linecap='round' stroke-linejoin='round' stroke-width='2.5' d='M2 5.7L7.0541 10.5L16 2'/%3e%3c/svg%3e")`,
+  backgroundRepeat: "no-repeat",
+  backgroundPositionX: "center",
+  backgroundPositionY: "center",
+  _checked: {
+    backgroundColor: "primary",
+    borderColor: "primary",
+    backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' width='18' height='12' viewBox='0 0 18 12'%3e%3cpath stroke='%23FFF' stroke-linecap='round' stroke-linejoin='round' stroke-width='2.5' d='M2 5.7L7.0541 10.5L16 2'/%3e%3c/svg%3e")`,
+  },
+});
