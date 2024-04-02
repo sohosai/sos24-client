9962b63081bc39efa041f5e38e868c240e4ef33c
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-03 00:17:16 +0900

    fix: 新規登録実装

--- src/app/signup/SignupForm.tsx
+++ src/app/signup/SignupForm.tsx
@@ -3,35 +3,44 @@
 import { redirect } from "next/navigation";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import { css, cx } from "@styled-system/css";
+import { basicErrorMessageStyle, basicFormStyle, checkboxFormStyle } from "@/components/forms/styles";
+import { Button } from "@/components/Button";
+import { CheckboxForm } from "@/components/forms/Checkbox";
 
 type CreateUserInput = {
   name: string;
   kana_name: string;
 
   phone_number: string;
-  category: "undergraduate" | "graduate" | "academic_staff";
 
   email: string;
   password: string;
+
+  agreement: boolean
 };
 
+let labelAndInputStyle = css({ display: "flex", flexDir: "column", gap: "6px", width: "100%" });
 export const SignupForm = () => {
-  const { register, handleSubmit } = useForm<CreateUserInput>();
+  const { register, handleSubmit, formState: { errors }, setError } = useForm<CreateUserInput>();
 
   const onSubmit = async (data: CreateUserInput) => {
+    if (!data.agreement) {
+      toast.error("利用規約に同意してください");
+      return;
+    }
     const resp = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/users`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         name: data.name,
         kana_name: data.kana_name,
         phone_number: data.phone_number,
-        category: data.category,
         email: data.email,
-        password: data.password,
-      }),
+        password: data.password
+      })
     }).then((res) => res.status);
 
     if (resp === 201) {
@@ -41,36 +50,55 @@ export const SignupForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="email">メールアドレス</label>
-        <input type="email" id="email" {...register("email")} />
+    <form onSubmit={handleSubmit(onSubmit)}
+          className={css({ display: "flex", flexDir: "column", alignItems: "center", gap: 4, width: 80 })}>
+      <div className={labelAndInputStyle}>
+        <label htmlFor="name" className={css({ fontWeight: "bold" })}>名前</label>
+        <input type="text" id="name" placeholder="嘉納治五郎" className={cx(basicFormStyle())} {...register("name", {
+          required: "名前を入力してください"
+        })} />
+        {errors.name && <span className={basicErrorMessageStyle}>{errors.name.message}</span>}
       </div>
-      <div>
-        <label htmlFor="password">パスワード</label>
-        <input type="password" id="password" {...register("password")} />
+      <div className={labelAndInputStyle}>
+        <label htmlFor="kana_name" className={css({ fontWeight: "bold" })}>なまえ</label>
+        <input type="text" id="kana_name" placeholder="かのうじごろう"
+               className={cx(basicFormStyle())} {...register("kana_name", {
+          required: "名前のふりがなを入力してください",
+          pattern: { value: /^[ぁ-んー－゛゜]+$/, message: "ひらがなで入力してください" }
+        })} />
+        {errors.kana_name && <span className={basicErrorMessageStyle}>{errors.kana_name.message}</span>}
       </div>
-      <div>
-        <label htmlFor="name">名前</label>
-        <input type="text" id="name" {...register("name")} />
+      <div className={labelAndInputStyle}>
+        <label htmlFor="email" className={css({ fontWeight: "bold" })}>メールアドレス</label>
+        <input type="email" id="email" placeholder="xxxx@x.tsukuba.ac.jp"
+               className={cx(basicFormStyle())} {...register("email", {
+          required: "メールアドレスを入力してください",
+          pattern: { value: /.*@.*\.tsukuba\.ac\.jp$/, message: "筑波大学のメールアドレスを入力してください" }
+        })} />
+        <span className={css({ fontSize: "sm", color: "gray.600" })}>tsukuba.ac.jpで終わるものを入力してください</span>
+        {errors.email && <span className={basicErrorMessageStyle}>{errors.email.message}</span>}
       </div>
-      <div>
-        <label htmlFor="kana_name">フリガナ</label>
-        <input type="text" id="kana_name" {...register("kana_name")} />
+      <div className={labelAndInputStyle}>
+        <label htmlFor="password" className={css({ fontWeight: "bold" })}>パスワード</label>
+        <input type="password" id="password"
+               className={cx(basicFormStyle())} {...register("password", { required: "パスワードを入力してください" })} />
+        {errors.password && <span className={basicErrorMessageStyle}>{errors.password.message}</span>}
       </div>
-      <div>
-        <label htmlFor="phone_number">電話番号</label>
-        <input type="tel" id="phone_number" {...register("phone_number")} />
+      <div className={labelAndInputStyle}>
+        <label htmlFor="phone_number" className={css({ fontWeight: "bold" })}>電話番号</label>
+        <input type="tel" id="phone_number"
+               className={cx(basicFormStyle())} {...register("phone_number", { required: "電話番号を入力してください" })} />
+        {errors.phone_number && <span className={basicErrorMessageStyle}>{errors.phone_number.message}</span>}
       </div>
-      <div>
-        <label htmlFor="category">所属</label>
-        <select id="category" {...register("category")}>
-          <option value="undergraduate">学類生</option>
-          <option value="graduate">大学院生</option>
-          <option value="academic_staff">教職員</option>
-        </select>
+      <div className={css({ display: "flex", alignItems: "center", gap: 3 })}>
+        <input type="checkbox" id="agreement"
+               className={checkboxFormStyle} {...register("agreement", { required: "利用規約に同意してください" })} />
+        <label
+          htmlFor="agreement">利用規約に同意する</label>
       </div>
-      <button type="submit">登録</button>
+      {errors.agreement && <span className={basicErrorMessageStyle}>{errors.agreement.message}</span>}
+      <Button color="primary" className={css({ alignSelf: "center" })} type="submit">送信
+      </Button>
     </form>
   );
 };
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,6 +5,9 @@ import { SignupForm } from "./SignupForm";
 import NextLink from "next/link";
 import { useAuthState } from "@/lib/firebase";
 import { redirect } from "next/navigation";
+import { css } from "@styled-system/css";
+import Image from "next/image";
+import Triangle from "@/app/signin/Triangle.svg";
 
 const SignupPage: NextPage = () => {
   const { user, isLoading } = useAuthState();
@@ -14,16 +17,31 @@ const SignupPage: NextPage = () => {
   }
 
   return (
-    <div>
-      <h1>新規登録</h1>
+    <div className={css({
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDir: "column",
+      width: "100%",
+      height: "calc(100vh - token(spacing.20))"
+    })}>
+      <h1 className={css({
+        fontSize: "2xl",
+        fontWeight: "bold"
+      })}>新規登録</h1>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
         <>
           <SignupForm />
-          <p>
-            すでにアカウントをお持ちの場合は <NextLink href="/signin">サインイン</NextLink> してください
-          </p>
+          <div className={css({ marginTop: 4, display: "flex", gap: 3.5 })}>
+            <Image src={Triangle} alt={"三角形のアイコン"} />
+            <NextLink href="/signup"
+                      className={css({
+                        textDecoration: "underline",
+                        fontWeight: "bold"
+                      })}>新規アカウント登録はこちら</NextLink>
+          </div>
         </>
       )}
     </div>
