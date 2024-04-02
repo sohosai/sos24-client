88ae9c1154da92cd95933ebc354b7f184d26fb02
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-02 12:45:57 +0900

    feat: SignIn ページ実装

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,6 +5,9 @@ import { SigninForm } from "./SignInPage";
 import NextLink from "next/link";
 import { redirect } from "next/navigation";
 import { useAuthState } from "@/lib/firebase";
+import { css } from "@styled-system/css";
+import Image from "next/image";
+import Triangle from "./Triangle.svg";
 
 const SigninPage: NextPage = () => {
   const { user, isLoading } = useAuthState();
@@ -12,20 +15,38 @@ const SigninPage: NextPage = () => {
   if (!isLoading && user) {
     redirect("/dashboard");
   }
-
   return (
-    <div>
-      <h1>ログイン</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <SigninForm />
-          <p>
-            アカウントをお持ちでない場合は <NextLink href="/signup">新規登録</NextLink> してください
-          </p>
-        </>
-      )}
+    <div className={css({
+      display: "flex", justifyContent: "center", alignItems: "center", width: "100%",
+      height: "calc(100vh - token(spacing.20))"
+    })}>
+      <div className={css({
+        display: "flex",
+        flexDir: "column",
+        alignItems: "center",
+        boxShadow: "token(shadows.lg)",
+        paddingY: 11,
+        paddingX: 20,
+        borderRadius: "token(xl)",
+        width: "fit-content"
+      })}>
+        <h1 className={css({ fontSize: "2xl", fontWeight: "bold", marginBottom: 8 })}>ログイン</h1>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <SigninForm />
+            <div className={css({ marginTop: 4, display: "flex", gap: 3.5 })}>
+              <Image src={Triangle} alt={"三角形のアイコン"} />
+              <NextLink href="/signup"
+                        className={css({
+                          textDecoration: "underline",
+                          fontWeight: "bold"
+                        })}>新規アカウント登録はこちら</NextLink>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
