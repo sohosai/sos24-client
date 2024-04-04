d13a3f3770acd145a56b9934f7a48db3c3bfa1fe
Arata <arata04962@gmail.com>
2024-04-05 04:46:38 +0900

    feat: dashboardにお知らせ一覧へのリンクを追加した

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,16 +2,37 @@
 
 import {NextPage} from "next";
 import {NewsView} from "@/components/news/NewsView";
-import {container, stack} from "@styled-system/patterns";
+import {container, flex, stack} from "@styled-system/patterns";
 import {Title} from "@/components/Title";
+import Link from "next/link";
+import Image from "next/image";
+
+import pulldownIcon from "../../components/assets/Pulldown.svg";
+import {css} from "@styled-system/css";
 
 const DashboardPage: NextPage = () => {
   return (
     <div className={container()}>
       <div className={stack({gap: 8, marginY: 8})}>
-        <div className={stack({gap: 6})}>
+        <div className={stack({gap: 6, alignItems: "center"})}>
           <div>
             <Title>お知らせ</Title>
+          </div>
+          <div className={flex({position: "absolute", top: 24, justifyContent: "flex-end", width: "90%"})}>
+            <Link href="/news" className={flex({
+              backgroundColor: "sohosai.purple",
+              borderRadius: 2,
+              paddingX: 4,
+              paddingY: 1,
+              gap: 2
+            })}>
+              <Image src={pulldownIcon} alt=""/>
+              <span className={css({
+                color: "white",
+                fontSize: "xs",
+                fontWeight: "bold"
+              })}>お知らせ一覧へ</span>
+            </Link>
           </div>
           <NewsView/>
         </div>
