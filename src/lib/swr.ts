963f635a7f3c650bbae286d70dbb2597e5fca98a
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-05 01:10:09 +0900

    fix: SWRでfetchしたときエラーが発生した場合にはerrorに値が入るよう修正

--- src/lib/swr.ts
+++ src/lib/swr.ts
@@ -1,15 +1,22 @@
 export const fetcherWithToken = async (url: string, token?: string, init?: RequestInit) => {
   if (!token) {
-    return {ok: false, statusCode: 401, json: ""}
+    return { ok: false, statusCode: 401, json: "" };
   }
 
   return await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}${url}`, {
     headers: {
-      Authorization: `Bearer ${token}`,
-    },
+      Authorization: `Bearer ${token}`
+    }
   })
     .then(async (res) => {
-      return {ok: res.ok, statusCode: res.status, json: await res.json()}
+      const resObject  = await res.json();
+      if (!res.ok) {
+        const error = new Error(res.statusText);
+        error.name = resObject.code;
+        error.message = resObject.message;
+        throw error;
+      }
+      return { ok: res.ok, statusCode: res.status, json: await resObject };
     })
     .catch((error) => {
       throw new Error(error);
