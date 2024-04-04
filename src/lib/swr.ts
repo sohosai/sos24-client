1a100f9133b1b2f8e25e00d13e97d43c662e92f3
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-05 01:31:20 +0900

    fix: 未認証時もerrorを返す

--- src/lib/swr.ts
+++ src/lib/swr.ts
@@ -1,6 +1,7 @@
 export const fetcherWithToken = async (url: string, token?: string, _init?: RequestInit) => {
   if (!token) {
-    return { ok: false, statusCode: 401, json: "" };
+    const error = new Error("Not Authorized");
+    throw error;
   }
 
   return await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}${url}`, {
