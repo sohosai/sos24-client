7e288e3aa40fc6d319042d5af68477a59dcebb20
Arata <arata04962@gmail.com>
2024-04-05 04:46:52 +0900

    feat: お知らせ一覧ページの横幅を調整した

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {container, stack} from "@styled-system/patterns";
+import {center, container, stack} from "@styled-system/patterns";
 import {css} from "@styled-system/css";
 import {NewsView} from "@/components/news/NewsView";
 
@@ -9,7 +9,11 @@ const NewsPage = () => {
     <div className={container()}>
       <div className={stack({gap: 8, marginY: 8})}>
         <h2 className={css({fontSize: "2xl", fontWeight: "bold"})}>お知らせ一覧</h2>
-        <NewsView/>
+        <div className={center()}>
+          <div className={css({width: "90%"})}>
+            <NewsView/>
+          </div>
+        </div>
       </div>
     </div>
   );
