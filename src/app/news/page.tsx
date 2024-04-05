bbe26bd9d76590b998b27f0e9a5e2f0ab826a302
Arata <arata04962@gmail.com>
2024-04-05 18:19:27 +0900

    style: npm run format

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import {center, container, stack} from "@styled-system/patterns";
-import {css} from "@styled-system/css";
-import {NewsView} from "@/components/news/NewsView";
+import { center, container, stack } from "@styled-system/patterns";
+import { css } from "@styled-system/css";
+import { NewsView } from "@/components/news/NewsView";
 
 const NewsPage = () => {
   return (
     <div className={container()}>
-      <div className={stack({gap: 8, marginY: 8})}>
-        <h2 className={css({fontSize: "2xl", fontWeight: "bold"})}>お知らせ一覧</h2>
+      <div className={stack({ gap: 8, marginY: 8 })}>
+        <h2 className={css({ fontSize: "2xl", fontWeight: "bold" })}>お知らせ一覧</h2>
         <div className={center()}>
-          <div className={css({width: "90%"})}>
-            <NewsView/>
+          <div className={css({ width: "90%" })}>
+            <NewsView />
           </div>
         </div>
       </div>
