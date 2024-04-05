a3d7a925154b64c247ef2fe88c714a70faba8047
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-06 01:12:15 +0900

    feat: Project Viewを作成

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,21 +5,28 @@ import useSWR from "swr";
 import { assignType } from "@/lib/openapi";
 import { NewsList } from "./NewsList";
 import { css } from "@styled-system/css";
+import { ProjectView } from "@/app/dashboard/ProjectView";
+import { Title } from "@/components/Title";
+import { container } from "@styled-system/patterns";
 
 const DashboardPage: NextPage = () => {
   const { data: newsRes } = useSWR("/news");
   const news = newsRes ? assignType("/news", newsRes) : undefined;
 
   return (
-    <div
-      className={css({
-        padding: 5,
-      })}>
-      {!newsRes?.ok ? (
-        <p>お知らせの取得中にエラーが発生しました(エラー: {String(newsRes?.statusCode)})</p>
-      ) : (
-        <NewsList newsList={news ?? []} />
-      )}
+    <div className={container()}>
+      <Title>企画情報</Title>
+      <ProjectView />
+      <div
+        className={css({
+          padding: 5,
+        })}>
+        {!newsRes?.ok ? (
+          <p>お知らせの取得中にエラーが発生しました(エラー: {String(newsRes?.statusCode)})</p>
+        ) : (
+          <NewsList newsList={news ?? []} />
+        )}
+      </div>
     </div>
   );
 };
