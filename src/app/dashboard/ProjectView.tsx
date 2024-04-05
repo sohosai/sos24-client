180d5dca1dc1c6c599af55d4f896c51b9f69ce72
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-06 03:02:22 +0900

    feat: 副企画責任者招待リンクコピー

--- src/app/dashboard/ProjectView.tsx
+++ src/app/dashboard/ProjectView.tsx
@@ -1,9 +1,10 @@
 import useSWR from "swr";
-import { assignType } from "@/lib/openapi";
+import { assignType, client } from "@/lib/openapi";
 import React, { ReactNode } from "react";
 import { categoryToLabel } from "@/components/news/CategoryBadges";
 import { css, cx } from "@styled-system/css";
 import { basicErrorMessageStyle } from "@/components/forms/styles";
+import toast from "react-hot-toast";
 
 const TableCellStyle = css({
   paddingX: 14,
@@ -46,6 +47,35 @@ const TableRow = ({ label, value }: { label: ReactNode; value: ReactNode }) => (
 export const ProjectView: React.FC = () => {
   const { data: rawProjectData, error: projectErr, isLoading: projectIsLoading } = useSWR("/projects/me");
   const projectData = assignType("/projects/me", rawProjectData);
+  // positionの型はschemaの型を使いたい
+  const hanleCopyInviteLink = (project_id: string, position: "owner" | "sub_owner") => {
+    client
+      .POST("/invitations", {
+        body: {
+          project_id,
+          position,
+        },
+      })
+      .then((res) => {
+        const data = [
+          new ClipboardItem({
+            "text/plain": new Blob([`${document.location.origin}/invitations/${res.data?.id}`], { type: "text/plain" }),
+          }),
+        ];
+        navigator.clipboard
+          .write(data)
+          .then(() => {
+            toast.success("招待リンクをコピーしました");
+          })
+          .catch(() => {
+            toast.error("招待リンクのコピーに失敗しました");
+          });
+      })
+      .catch((e) => {
+        toast.error("招待リンクの作成に失敗しました");
+        throw new Error(e);
+      });
+  };
   return (
     <>
       {projectIsLoading ? (
@@ -63,8 +93,36 @@ export const ProjectView: React.FC = () => {
                 <TableRow label="企画団体名（ふりがな）" value={projectData.kana_group_name} />
                 <TableRow label="企画責任者" value={projectData.owner_name} />
                 <TableRow
-                  label="副企画責任者"
-                  value={projectData.sub_owner_name ?? <span className={css({ color: "red.800" })}>未設定</span>}
+                  label={
+                    <span
+                      className={css(
+                        !projectData.sub_owner_name && {
+                          position: "relative",
+                          _after: {
+                            content: '""',
+                            position: "absolute",
+                            top: "-50%",
+                            right: "-20%",
+                            width: 3,
+                            height: 3,
+                            backgroundColor: "error",
+                            borderRadius: "full",
+                            display: "block",
+                          },
+                        },
+                      )}>
+                      副企画責任者
+                    </span>
+                  }
+                  value={
+                    projectData.sub_owner_name ?? (
+                      <button
+                        className={css({ color: "sohosai.purple", textDecoration: "underline", cursor: "pointer" })}
+                        onClick={() => hanleCopyInviteLink(projectData.id, "sub_owner")}>
+                        登録リンクをコピー
+                      </button>
+                    )
+                  }
                 />
                 <TableRow label="企画区分" value={categoryToLabel(projectData.category)} />
                 <TableRow label="企画属性" value={projectData.attributes} />
