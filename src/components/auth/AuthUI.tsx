d1b8718d453c1fc7beb7fa0e60b05ddf0bf8e05c
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-05 16:18:49 +0900

    feat: メール認証を必須に

--- src/components/auth/AuthUI.tsx
+++ src/components/auth/AuthUI.tsx
@@ -6,6 +6,7 @@ import { atom, useAtomValue } from "jotai";
 import { useAuthState } from "@/lib/firebase";
 import { Loading } from "@/components/Loading";
 import { css } from "@styled-system/css";
+import { EmailVerification } from "@/components/auth/EmailVerification";
 
 export const authModeAtom = atom<"signIn" | "signUp">("signIn");
 
@@ -23,7 +24,7 @@ export const AuthUI: FC<PropsWithChildren> = ({ children }) => {
           <Loading />
         </div>
       ) : authState.user ? (
-        children
+        <>{authState.user.emailVerified ? { children } : <EmailVerification />}</>
       ) : (
         <>{authMode === "signIn" ? <SigninPage /> : <SignupPage />}</>
       )}
