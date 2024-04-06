c8ed235c05df2f12afef288fbed1fc3371cb40b3
Arata <arata04962@gmail.com>
2024-04-06 22:29:00 +0900

    feat: 実委人向けのお知らせ詳細ページを実装した

---
+++ src/app/committee/news/[news_id]/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import useSWR from "swr";
+import { assignType } from "@/lib/openapi";
+import { container } from "@styled-system/patterns";
+import { News } from "@/components/news/News";
+import { NextPage } from "next";
+
+const CommitteeNewsDetailsPage: NextPage<{ params: { news_id: string } }> = ({ params }) => {
+  const { data, error, isLoading } = useSWR(`/news/${params.news_id}`);
+  if (isLoading) {
+    return;
+  }
+  if (error) {
+    switch (error.name) {
+      case "news/not-found":
+        return <p>このお知らせは存在しません。</p>;
+      default:
+        return <p>招待の読み込み中に不明なエラーが発生しました。</p>;
+    }
+  }
+
+  const news = assignType("/news/{news_id}", data);
+
+  return (
+    <div className={container({ maxWidth: "4xl" })}>
+      <News news={news} isCommittee />
+    </div>
+  );
+};
+
+export default CommitteeNewsDetailsPage;
