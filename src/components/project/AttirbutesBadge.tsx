165b40d88dbd42ec8ab6cb774bdc72426cde87f6
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-07 18:10:21 +0900

    refactor: 企画属性

--- src/components/project/AttirbutesBadge.tsx
+++ src/components/project/AttirbutesBadge.tsx
@@ -1,6 +1,33 @@
 import { components } from "@/schema";
 import { center, hstack } from "@styled-system/patterns";
 import { css, cx } from "@styled-system/css";
+import { AttributesFormatter } from "./AttributesFormatter";
+
+const attrbutesData: {
+  rawString: components["schemas"]["ProjectAttribute"];
+  string: string;
+}[] = [
+  {
+    rawString: "inside",
+    string: "内",
+  },
+  {
+    rawString: "outside",
+    string: "外",
+  },
+  {
+    rawString: "academic",
+    string: "学",
+  },
+  {
+    rawString: "art",
+    string: "芸",
+  },
+  {
+    rawString: "official",
+    string: "委",
+  },
+];
 
 export const ProjectAttributesBadge: React.FC<{
   attributes: components["schemas"]["ProjectAttribute"][];
@@ -21,21 +48,14 @@ export const ProjectAttributesBadge: React.FC<{
   });
   return (
     <ul className={hstack()}>
-      <li className={cx(ItemStyle, attributes.includes("inside") && ActiveItemStyle)} title="屋内">
-        内
-      </li>
-      <li className={cx(ItemStyle, attributes.includes("outside") && ActiveItemStyle)} title="屋外">
-        外
-      </li>
-      <li className={cx(ItemStyle, attributes.includes("academic") && ActiveItemStyle)} title="学術認定企画">
-        学
-      </li>
-      <li className={cx(ItemStyle, attributes.includes("art") && ActiveItemStyle)} title="芸術祭参加企画">
-        芸
-      </li>
-      <li className={cx(ItemStyle, attributes.includes("official") && ActiveItemStyle)} title="委員会開催企画">
-        委
-      </li>
+      {attrbutesData.map((e) => (
+        <li
+          className={cx(ItemStyle, attributes.includes(e.rawString) && ActiveItemStyle)}
+          title={AttributesFormatter({ category: e.rawString })}
+          key={e.rawString}>
+          {e.string}
+        </li>
+      ))}
     </ul>
   );
 };
