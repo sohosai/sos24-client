9bbcec4ea111ae0a4237ae50b9916bd580eb6b90
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-07 12:46:25 +0900

    feat: 企画一覧ページ

---
+++ src/app/committee/projects/[project_id]/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { NextPage } from "next";
+import { center, container, stack } from "@styled-system/patterns";
+import { css } from "@styled-system/css";
+import { NewsView } from "@/components/news/NewsView";
+
+const CommitteeNewsPage: NextPage = () => {
+  return (
+    <div className={container()}>
+      <div className={stack({ gap: 8, marginY: 8 })}>
+        <h2 className={css({ fontSize: "2xl", fontWeight: "bold" })}>お知らせ一覧</h2>
+        <div className={center()}>
+          <div className={css({ width: "90%" })}>
+            <NewsView isCommittee />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommitteeNewsPage;
