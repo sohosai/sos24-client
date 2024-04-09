dca933710f15e3f6344ac99283839290cf28e05c
s7tya <53410646+s7tya@users.noreply.github.com>
2024-04-10 01:30:55 +0900

    非表示機能の実装

--- src/app/forms/FormsList.tsx
+++ src/app/forms/FormsList.tsx
@@ -6,6 +6,12 @@ import { components } from "@/schema";
 
 import { SubmitStatusBadge } from "@/components/SubmitStatus";
 import { getSubmitStatus, getTimeLeftText } from "@/lib/formHelpers";
+import { useAtom } from "jotai";
+import { hiddenFormIdsAtom } from "./page";
+
+import EyesOpenIcon from "@/components/assets/EyesOpen.svg"
+import EyesClosedIcon from "@/components/assets/EyesClosed.svg"
+import Image from "next/image";
 
 type Form = components["schemas"]["FormSummary"];
 type Answer = components["schemas"]["FormAnswerSummary"];
@@ -15,13 +21,15 @@ export const FormsList: FC<{
   answers: Answer[];
   filterUnsubmitted: boolean;
 }> = ({ forms, answers, filterUnsubmitted }) => {
+  const [hiddenFormIds, setHiddenFormIds] = useAtom(hiddenFormIdsAtom);
+
   return (
     <div
       className={css({
         width: "full",
         display: "grid",
         alignItems: "center",
-        gridTemplateColumns: "1fr 1fr 1fr 3fr 2fr",
+        gridTemplateColumns: "1fr 1fr 1fr 1fr 3fr 2fr",
         "& > * > *": {
           pr: 4,
           lineHeight: 2,
@@ -37,6 +45,7 @@ export const FormsList: FC<{
             borderBottom: "1px solid",
           },
         })}>
+        <div>非表示</div>
         <div>状態</div>
         <div>配信日</div>
         <div>締切日</div>
@@ -57,21 +66,44 @@ export const FormsList: FC<{
           return;
         }
 
+        const isShown = hiddenFormIds.includes(form.id);
+
         return (
-          <a
+          <div
             key={form.id}
-            href={`/forms/${form.id}`}
             className={css({
               display: "contents",
-            })}>
-            <div className={css({ paddingBlock: 2 })}>
-              <SubmitStatusBadge status={status} />
-            </div>
-            <div>{startsAt.format("YYYY/MM/DD")}</div>
-            <div>{endsAt.format("YYYY/MM/DD")}</div>
-            <div>{form.title}</div>
-            <div>{getTimeLeftText(dayjs(), endsAt, status)}</div>
-          </a>
+            })}
+          >
+            <button onClick={() => {
+              setHiddenFormIds(prev => {
+                if (prev.includes(form.id)) {
+                  return prev.filter((i) => i != form.id)
+                } else {
+                  return [...prev, form.id]
+                }
+              })
+            }}>
+              {isShown ? (
+              <Image src={EyesClosedIcon} alt="非表示" />
+              ) : (
+              <Image src={EyesOpenIcon} alt="表示" />
+              )}
+            </button>
+            <a
+              href={`/forms/${form.id}`}
+              className={css({
+                display: "contents",
+              })}>
+              <div className={css({ paddingBlock: 2 })}>
+                <SubmitStatusBadge status={status} />
+              </div>
+              <div>{startsAt.format("YYYY/MM/DD")}</div>
+              <div>{endsAt.format("YYYY/MM/DD")}</div>
+              <div>{form.title}</div>
+              <div>{getTimeLeftText(dayjs(), endsAt, status)}</div>
+            </a>
+          </div>
         );
       })}
     </div>
