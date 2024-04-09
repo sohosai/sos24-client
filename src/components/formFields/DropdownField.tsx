09893d1236e9b811a5314b7078943b383a068fba
OKAMOTO Shigehiro <primary@goahi.live>
2024-04-09 22:58:21 +0900

    fix dropdown

--- src/components/formFields/DropdownField.tsx
+++ src/components/formFields/DropdownField.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { RequiredBadge } from "./_components/RequiredBadge";
 import { css, cx } from "@styled-system/css";
-import { basicFormLabelStyle, basicFormStyle, dropdownStyle } from "./styles";
+import { basicFormLabelStyle, basicFormStyle, dropdownStyle, basicErrorMessageStyle } from "./styles";
 import { basicFieldProps } from "./_components/types";
 
 interface Props extends basicFieldProps {
@@ -15,7 +15,9 @@ export const DropdownForm: FC<Props> = (props: Props) => {
         {props.label}
         {props.required !== undefined && <RequiredBadge isRequired={props.required} className={css({ marginInline: 2 })} />}
       </label>
-      <select id={props.id} name={props.id} className={cx(basicFormStyle(), dropdownStyle)}>
+      <select id={props.id}
+      {...props.register}
+      className={cx(basicFormStyle(), dropdownStyle)}>
         <option value="" hidden></option>
         {props.options.map((option) => {
           return (
@@ -25,6 +27,16 @@ export const DropdownForm: FC<Props> = (props: Props) => {
           );
         })}
       </select>
+      <div className={css({ sm: { marginInline: 3 }, marginBlock: 1 })}>
+        <p
+          className={css({
+            fontSize: "sm",
+            color: "gray.500",
+          })}>
+          {props.description}
+        </p>
+        {props.error && <span className={basicErrorMessageStyle}>{props.error}</span>}
+      </div>
     </div>
   );
 };
