45b3edc64e10bcde52c5bc2516b78b00e9e93c41
s7tya <53410646+s7tya@users.noreply.github.com>
2024-04-10 01:31:25 +0900

    format

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -12,7 +12,7 @@ import { NotificationBadge } from "@/components/NotificationBadge";
 import { atomWithStorage } from "jotai/utils";
 import { useAtomValue } from "jotai";
 
-export const hiddenFormIdsAtom = atomWithStorage<string[]>('hiddenFormIds', []);
+export const hiddenFormIdsAtom = atomWithStorage<string[]>("hiddenFormIds", []);
 
 const DashboardPage: NextPage = () => {
   const { data: projectRes, error: projectResError, isLoading: projectResIsLoading } = useSWR("/projects/me");
@@ -32,7 +32,7 @@ const DashboardPage: NextPage = () => {
   } = useSWR(() => `/form-answers?project_id=` + project?.id);
   const answers = answersRes ? assignType("/form-answers", answersRes) : undefined;
 
-  const hiddenFormIds = useAtomValue(hiddenFormIdsAtom)
+  const hiddenFormIds = useAtomValue(hiddenFormIdsAtom);
 
   const isLoading = projectResIsLoading || formsResIsLoading || answersResIsLoading;
   const error = projectResError || formsResError || answersResError;
@@ -81,13 +81,24 @@ const DashboardPage: NextPage = () => {
           <Button color={filterUnsubmitted ? "primary" : "secondary"} onClick={toggleFilter} onTouchEnd={toggleFilter}>
             未提出のみ表示
           </Button>
-          <FormsList forms={forms.filter((form) => !hiddenFormIds.includes(form.id))} answers={answers} filterUnsubmitted={filterUnsubmitted} />
-          
-          <h3 className={css({
-            fontSize: "lg",
-            fontWeight: "bold"
-          })}>非表示中の申請</h3>
-          <FormsList forms={forms.filter((form) => hiddenFormIds.includes(form.id))} answers={answers} filterUnsubmitted={filterUnsubmitted} />
+          <FormsList
+            forms={forms.filter((form) => !hiddenFormIds.includes(form.id))}
+            answers={answers}
+            filterUnsubmitted={filterUnsubmitted}
+          />
+
+          <h3
+            className={css({
+              fontSize: "lg",
+              fontWeight: "bold",
+            })}>
+            非表示中の申請
+          </h3>
+          <FormsList
+            forms={forms.filter((form) => hiddenFormIds.includes(form.id))}
+            answers={answers}
+            filterUnsubmitted={filterUnsubmitted}
+          />
         </div>
       </div>
     </>
