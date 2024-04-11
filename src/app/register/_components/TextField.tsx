79ecb9b0fac680e37ab39e13cbbcb458adfa13f9
OKAMOTO Shigehiro <primary@goahi.live>
2024-04-12 00:33:32 +0900

    split out register form components

---
+++ src/app/register/_components/TextField.tsx
@@ -0,0 +1,55 @@
+import { css } from "@styled-system/css";
+import { basicErrorMessageStyle, basicFormLabelStyle, basicFormStyle } from "@/components/formFields/styles";
+import { center } from "@styled-system/patterns";
+
+import { RequiredBadge } from "@/components/formFields/_components/RequiredBadge";
+import type { basicFieldProps } from "@/components/formFields/_components/types";
+
+interface TextFieldProps extends basicFieldProps {
+  type: "text" | "textarea";
+  placeholder?: string;
+}
+
+export const TextField = ({ type, id, label, placeholder, description, required, error, register }: TextFieldProps) => {
+  return (
+    <div>
+      <label htmlFor={id} className={basicFormLabelStyle}>
+        {label}
+        {required !== undefined && <RequiredBadge isRequired={required} className={css({ marginInline: 2 })} />}
+      </label>
+      <div className={center()}>
+        <div className={css({ width: { base: "100%", sm: "90%" } })}>
+          {type === "text" ? (
+            <input
+              type="text"
+              id={id}
+              placeholder={placeholder}
+              {...register}
+              className={basicFormStyle({ isInvalid: !!error })}
+            />
+          ) : (
+            type === "textarea" && (
+              <textarea
+                id={id}
+                rows={5}
+                placeholder={placeholder}
+                {...register}
+                className={basicFormStyle({ isInvalid: !!error })}
+              />
+            )
+          )}
+          <div className={css({ sm: { marginInline: 3 }, marginBlock: 1 })}>
+            <p
+              className={css({
+                fontSize: "sm",
+                color: "gray.500",
+              })}>
+              {description}
+            </p>
+            {error && <span className={basicErrorMessageStyle}>{error}</span>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
