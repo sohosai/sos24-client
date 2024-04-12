7f8382499f4a701195bd917383e3c02bd1eba735
s7tya <53410646+s7tya@users.noreply.github.com>
2024-04-12 17:22:54 +0900

    ちょっとだけフォームをスタイリング

--- src/app/committee/forms/new/FormFieldEditor.tsx
+++ src/app/committee/forms/new/FormFieldEditor.tsx
@@ -1,44 +1,77 @@
 import { FC } from "react";
-import { CreateFormInput, type FormField } from "./page";
+import { CreateFormInput, textInputStyle, type FormField } from "./page";
 import { UseFormRegister } from "react-hook-form";
+import { checkboxFormStyle } from "@/components/formFields/styles";
+import { css } from "@styled-system/css";
 
 export const FormFieldEditor: FC<{ field: FormField; index: number; register: UseFormRegister<CreateFormInput> }> = ({
   field,
   index,
   register,
 }) => {
-  switch (field.type) {
-    case "string":
-      return (
-        <div>
-          <p>テキスト項目</p>
-          <label>
-            <input type="checkbox" {...register(`items.${index}.required`)} />
-            必須
-          </label>
-          <label>
-            <input type="checkbox" {...register(`items.${index}.allow_newline`)} />
-            複数行での回答
-          </label>
+  return (
+    <div
+      className={css({
+        border: "1px solid token(colors.gray.400)",
+        rounded: "md",
+        padding: 5,
+      })}>
+      {(() => {
+        switch (field.type) {
+          case "string":
+            return (
+              <>
+                <p>テキスト項目</p>
+                <label
+                  className={css({
+                    display: "inline-flex",
+                    alignItems: "center",
+                  })}>
+                  <input type="checkbox" {...register(`items.${index}.required`)} className={checkboxFormStyle} />
+                  必須
+                </label>
+                <label
+                  className={css({
+                    display: "inline-flex",
+                    alignItems: "center",
+                  })}>
+                  <input type="checkbox" {...register(`items.${index}.allow_newline`)} className={checkboxFormStyle} />
+                  複数行での回答
+                </label>
 
-          <label>質問</label>
-          <input {...register(`items.${index}.name`, { required: true })} />
+                <div>
+                  <label>質問</label>
+                  <input {...register(`items.${index}.name`, { required: true })} className={textInputStyle} />
+                </div>
 
-          <label>説明(任意)</label>
-          <input {...register(`items.${index}.description`)} />
+                <div>
+                  <label>説明(任意)</label>
+                  <textarea {...register(`items.${index}.description`)} className={textInputStyle} />
+                </div>
 
-          <label>最小文字数</label>
-          <input type="number" {...register(`items.${index}.min_length`, { valueAsNumber: true })} />
+                <label>最小文字数</label>
+                <input
+                  type="number"
+                  {...register(`items.${index}.min_length`, { valueAsNumber: true })}
+                  className={textInputStyle}
+                />
 
-          <label>最大文字数</label>
-          <input type="number" {...register(`items.${index}.max_length`, { valueAsNumber: true })} />
-        </div>
-      );
-    case "int":
-      return <></>;
-    case "choose_one":
-      return <></>;
-    case "choose_many":
-      return <></>;
-  }
+                <label>最大文字数</label>
+                <input
+                  type="number"
+                  {...register(`items.${index}.max_length`, { valueAsNumber: true })}
+                  className={textInputStyle}
+                />
+              </>
+            );
+          case "int":
+            return <></>;
+          case "choose_one":
+            return <></>;
+          case "choose_many":
+            return <></>;
+        }
+      })()}
+    </div>
+  );
 };
