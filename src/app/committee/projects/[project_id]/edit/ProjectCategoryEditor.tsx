c751f6e40d9d7da2a35b9ea78fea6c266af400cc
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-13 16:50:00 +0900

    refactor: ファイル分割

---
+++ src/app/committee/projects/[project_id]/edit/ProjectCategoryEditor.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { ProjectCategoryFormatter } from "@/components/ProjectCategoryFormatter";
+import { projectCategories } from "@/lib/valibot";
+import { css, cx } from "@styled-system/css";
+import { hstack, visuallyHidden } from "@styled-system/patterns";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+export const ProjectCategoryEditor: React.FC<{ register: UseFormRegisterReturn }> = ({ register }) => {
+  return (
+    <div className={hstack({ flexWrap: "wrap" })}>
+      {projectCategories.map((category) => (
+        <label
+          key={category}
+          className={cx(
+            css({
+              paddingBlock: 2,
+              paddingInline: 6,
+              borderRadius: "2xl",
+              cursor: "pointer",
+              color: "gray.600",
+              backgroundColor: "gray.200",
+              fontSize: "sm",
+              fontWeight: "bold",
+              boxSizing: "border-box",
+              "&:has(> input:checked)": {
+                color: "sohosai.purple",
+                border: "2px solid ",
+                backgroundColor: "white",
+              },
+            }),
+          )}>
+          <ProjectCategoryFormatter category={category} />
+          <input type="radio" value={category} className={visuallyHidden()} {...register} />
+        </label>
+      ))}
+    </div>
+  );
+};
