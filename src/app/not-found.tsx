85ec17da0276e9cfdd386478dc8144adadbf351b
WhatACotton <whatacotton@gmail.com>
2024-04-13 18:57:36 +0900

    add: 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,42 @@
+import sendIcon from "@/components/assets/notFoundSopotan.png";
+
+import Image, { StaticImageData } from "next/image";
+
+import notFoundSopotan from "@/components/assets/notFoundSopotan.png";
+import { css } from "@styled-system/css";
+import { box, center, hstack, vstack } from "@styled-system/patterns";
+export default function NotFoundPage() {
+  return (
+    <div className={center({ w: "100%", mt: 48 })}>
+      <div className={hstack({ gap: 24 })}>
+        <Image
+          src={notFoundSopotan}
+          alt=""
+          className={css({
+            height: 60,
+            width: 60,
+            mt: 5,
+          })}
+        />
+        <div className={box({ width: "390px" })}>
+          <div className={vstack({ gap: 12 })}>
+            <span className={css({ fontSize: "24px", fontWeight: "600" })}>お探しのページが見つかりません。</span>
+            <span
+              className={css({
+                textAlign: "left",
+                color: "#4C4C4C",
+                fontSize: "15px",
+                fontWeight: "700",
+                h: "87px",
+                lineHeight: "2",
+              })}>
+              アクセスしようとしたページは移動または削除されたか、 <br />
+              現在利用できない可能性があります。 <br />
+              また、URLにタイプミスがないか再度ご確認ください。
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
