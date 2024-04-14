9ff423acd29eebf54870677d317ac66422e92a6e
s7tya <53410646+s7tya@users.noreply.github.com>
2024-04-15 01:21:52 +0900

    ﾌﾟﾙ

--- src/app/committee/forms/FormsList.tsx
+++ src/app/committee/forms/FormsList.tsx
@@ -1,11 +1,11 @@
 import { css } from "@styled-system/css";
+import Link from "next/link"
 import dayjs from "dayjs";
 import { FC } from "react";
 
 import { components } from "@/schema";
 
 import { NoResultNotice } from "@/components/NoResultNotice";
-import Link from "next/link";
 import { getCommitteeTimeLeftText, getFormStatus } from "@/lib/formHelpers";
 import { FormStatusBadge } from "@/components/FormStatusBadge";
 
@@ -15,65 +15,72 @@ export const FormsList: FC<{
   forms: Form[];
 }> = ({ forms }) => {
   return (
-    <div
-      className={css({
+    <div>
+      <div className={css({
         width: "full",
         display: "grid",
-        alignItems: "center",
-        gridTemplateColumns: "1fr 1fr 1fr 3fr 2fr",
-        "& > * > *": {
-          pr: 4,
-          lineHeight: 2,
-        },
-      })}>
+        justifyContent: "flex-end"
+      })}><Link href="/committee/forms/new">+ 新規作成</Link></div>
       <div
         className={css({
-          display: "contents",
-          color: "gray.500",
-          fontSize: "sm",
-          "& > *": {
-            borderColor: "gray.500",
-            borderBottom: "1px solid",
+          width: "full",
+          display: "grid",
+          alignItems: "center",
+          gridTemplateColumns: "1fr 1fr 1fr 3fr 2fr",
+          "& > * > *": {
+            pr: 4,
+            lineHeight: 2,
           },
         })}>
-        <div>状態</div>
-        <div>配信日</div>
-        <div>締切日</div>
-        <div>タイトル</div>
-        <div>締切まで</div>
-      </div>
-      {forms.length == 0 && (
-        <div className={css({ gridColumn: "1/7" })}>
-          <NoResultNotice message="申請はありません" />
+        <div
+          className={css({
+            display: "contents",
+            color: "gray.500",
+            fontSize: "sm",
+            "& > *": {
+              borderColor: "gray.500",
+              borderBottom: "1px solid",
+            },
+          })}>
+          <div>状態</div>
+          <div>配信日</div>
+          <div>締切日</div>
+          <div>タイトル</div>
+          <div>締切まで</div>
         </div>
-      )}
-      {forms.map((form) => {
-        const startsAt = dayjs(form.starts_at);
-        const endsAt = dayjs(form.ends_at);
-        const status = getFormStatus(dayjs(), startsAt, endsAt);
+        {forms.length == 0 && (
+          <div className={css({ gridColumn: "1/7" })}>
+            <NoResultNotice message="申請はありません" />
+          </div>
+        )}
+        {forms.map((form) => {
+          const startsAt = dayjs(form.starts_at);
+          const endsAt = dayjs(form.ends_at);
+          const status = getFormStatus(dayjs(), startsAt, endsAt);
 
-        return (
-          <div
-            key={form.id}
-            className={css({
-              display: "contents",
-            })}>
-            <Link
-              href={`/committee/forms/${form.id}`}
+          return (
+            <div
+              key={form.id}
               className={css({
                 display: "contents",
               })}>
-              <div className={css({ paddingBlock: 2 })}>
-                <FormStatusBadge status={status} />
-              </div>
-              <div>{startsAt.format("YYYY/MM/DD")}</div>
-              <div>{endsAt.format("YYYY/MM/DD")}</div>
-              <div>{form.title}</div>
-              <div>{getCommitteeTimeLeftText(dayjs(), endsAt)}</div>
-            </Link>
-          </div>
-        );
-      })}
+              <Link
+                href={`/committee/forms/${form.id}`}
+                className={css({
+                  display: "contents",
+                })}>
+                <div className={css({ paddingBlock: 2 })}>
+                  <FormStatusBadge status={status} />
+                </div>
+                <div>{startsAt.format("YYYY/MM/DD")}</div>
+                <div>{endsAt.format("YYYY/MM/DD")}</div>
+                <div>{form.title}</div>
+                <div>{getCommitteeTimeLeftText(dayjs(), endsAt)}</div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
