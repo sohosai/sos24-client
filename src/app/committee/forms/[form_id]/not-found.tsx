e9a6123a2701369135b4fb8f032a5186d379aa66
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-18 12:23:07 +0900

    feat: 404ページの設定

---
+++ src/app/committee/forms/[form_id]/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFound } from "@/common_components/NotFound";
+
+export default function NotFoundPage() {
+  return <NotFound message="申請が見つかりません" />;
+}
--- src/app/committee/forms/[form_id]/page.tsx
+++ src/app/committee/forms/[form_id]/page.tsx
@@ -7,6 +7,7 @@ import { NextPage } from "next";
 import Link from "next/link";
 import useSWR from "swr";
 import { FormDetailedView } from "./FormDetailedView";
+import { NotFound } from "@/common_components/NotFound";
 
 export const runtime = "edge";
 
@@ -14,7 +15,15 @@ const FormDetailedPage: NextPage<{ params: { form_id: string } }> = ({ params })
   const { data: formData, isLoading: formIsLoading, error: formError } = useSWR(`/forms/${params.form_id}`);
   const form = assignType("/forms/{form_id}", formData);
   if (formIsLoading) return;
-  if (formError) return "エラーが発生しました";
+  if (formError) {
+    switch (formError.name) {
+      case "form/not-found":
+      case "form/invalid-uuid":
+        return <NotFound message="申請が見つかりません" />;
+      default:
+        return "申請の読み込み中にエラーが発生しました";
+    }
+  }
   return (
     <main className={cx(container({ maxWidth: "4xl", paddingY: 4 }))}>
       <div className={vstack({ gap: 4, alignItems: "start", width: "full" })}>
