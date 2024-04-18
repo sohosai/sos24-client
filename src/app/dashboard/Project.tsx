67f53dfbbe76aa0b7ac580fe8590054e243a9254
Ryoga-exe <git@ryoga.dev>
2024-04-18 17:41:22 +0900

    feat: add RegistrationProgress

--- src/app/dashboard/Project.tsx
+++ src/app/dashboard/Project.tsx
@@ -8,8 +8,11 @@ import { ProjectTableView } from "@/common_components/project/ProjectView";
 import { assignType } from "@/lib/openapi";
 import useSWR from "swr";
 import { basicErrorMessageStyle } from "@/common_components/formFields/styles";
+import { RegistrationProgress } from "@/common_components/RegistrationProgress";
+
 export const Project: React.FC = () => {
   const [editable, setEditable] = useState(false);
+  const [step, setStep] = useState<1 | 2 | 3 | 4 | 5>(1);
   const {
     data: rawProjectData,
     error: projectErr,
@@ -17,6 +20,38 @@ export const Project: React.FC = () => {
     mutate: mutateProject,
   } = useSWR("/projects/me");
   const projectData = assignType("/projects/me", rawProjectData);
+
+  const {
+    data: rawFormData,
+    error: formErr,
+    isLoading: formIsLoading,
+    mutate: mutateForm,
+  } = useSWR(projectIsLoading ? null : `/forms?project_id=${projectData.id}`);
+  const formData = assignType("/forms", rawFormData);
+
+  if (!formIsLoading && formData) {
+    let hasAnsweredEveryForm = true;
+    let hasAnsweredOathForm = false;
+    for (const data of formData) {
+      if (data.title === "誓約書提出フォーム") {
+        hasAnsweredOathForm = data.answer_id !== null;
+      } else if (data.answer_id === null) {
+        hasAnsweredEveryForm = false;
+      }
+    }
+
+    if (hasAnsweredEveryForm) {
+      if (hasAnsweredOathForm) {
+        if (projectData.sub_owner_id !== null) {
+          setStep(5);
+        }
+        setStep(4);
+      } else {
+        setStep(3);
+      }
+      setStep(hasAnsweredOathForm ? 3 : 2);
+    }
+  }
 
   return (
     <>
@@ -34,14 +69,17 @@ export const Project: React.FC = () => {
             {projectErr ? (
               <div className={basicErrorMessageStyle}>企画取得に失敗しました</div>
             ) : (
-              <ProjectTableView
-                isEditMode={editable}
-                onSubmit={() => {
-                  mutateProject(rawProjectData);
-                  setEditable(false);
-                }}
-                projectData={projectData}
-              />
+              <>
+                <ProjectTableView
+                  isEditMode={editable}
+                  onSubmit={() => {
+                    mutateProject(rawProjectData);
+                    setEditable(false);
+                  }}
+                  projectData={projectData}
+                />
+                <RegistrationProgress step={step} />
+              </>
             )}
           </>
         )}
