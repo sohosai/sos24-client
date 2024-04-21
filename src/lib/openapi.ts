3816b444a149cd68749399618060efe02f3cfbc6
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-21 19:00:47 +0900

    update: 権限不足時も報告しない

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -27,7 +27,7 @@ const authMiddleware: Middleware = {
 
 const sentryMiddleware: Middleware = {
   async onResponse(res) {
-    if (!res.ok && res.status != 404 && res.status != 401) {
+    if (res.status !== 404 && res.status !== 403 && res.status !== 401) {
       const resErr = await res.json();
       const err = new Error(resErr.message);
       err.name = resErr.code;
