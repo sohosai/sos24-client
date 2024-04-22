d8d7d49e4f93ebda49ab24709e8f84a35db82a3a
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-04-22 10:18:36 +0900

    Revert "fix"
    This reverts commit 57c691a3966f3fb16ab8aef657cfb9f04e408e67.

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -27,15 +27,11 @@ const authMiddleware: Middleware = {
 
 const sentryMiddleware: Middleware = {
   async onResponse(res) {
-    if (res.status !== 404 && res.status !== 403 && res.status !== 401 && !res.ok) {
-      try {
-        const resErr = await res.json();
-        const err = new Error(resErr.message);
-        err.name = resErr.code;
-        captureException(err);
-      } catch {
-        captureException(res);
-      }
+    if (res.status !== 404 && res.status !== 403 && res.status !== 401) {
+      const resErr = await res.json();
+      const err = new Error(resErr.message);
+      err.name = resErr.code;
+      captureException(err);
     }
     return res;
   },
