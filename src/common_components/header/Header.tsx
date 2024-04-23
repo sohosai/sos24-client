3c6d5cd985713a26c4a30a0214a6f84f43e7b54b
WhatACotton <118359411+WhatACotton@users.noreply.github.com>
2024-04-23 10:19:36 +0900

    Update src/common_components/header/Header.tsx
    Co-authored-by: Arata <49086220+arata-nvm@users.noreply.github.com>

--- src/common_components/header/Header.tsx
+++ src/common_components/header/Header.tsx
@@ -132,7 +132,7 @@ export const Header: FC = () => {
         setShowMobileMenu(false);
       })(),
       {
-        loading: "サイン・アウトしています",
+        loading: "サインアウトしています",
         success: "サインアウトしました",
         error: "サインアウトできませんでした",
       },
