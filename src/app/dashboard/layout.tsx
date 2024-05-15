6ed5e3e04125f9ccb9eaf1f48c95c3475dd03b5d
Ryoga-exe <git@ryoga.dev>
2024-05-15 14:03:23 +0900

    feat: set metadata to /dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import RootLayout from "@/app/layout";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ダッシュボード",
+  description: "",
+};
+
+export default RootLayout;
