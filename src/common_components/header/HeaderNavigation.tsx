10c42c22ce6ceaf260da1b56fe1e85e58f178db9
naohanpen <github@social.naohanpen.jp>
2024-06-09 16:52:35 +0900

    fix#372 スマホ向けログアウトボタンの追加

--- src/common_components/header/HeaderNavigation.tsx
+++ src/common_components/header/HeaderNavigation.tsx
@@ -6,10 +6,18 @@ import { MenuData } from "./Header";
 import { hstack } from "@styled-system/patterns";
 import useSWR from "swr";
 import { assignType } from "@/lib/openapi";
+import style_HeaderNavigation from "@/common_components/header/HeaderNavigation.module.scss";
+import { useAuthState } from "@/lib/firebase";
 
-export const HeaderNavigation: FC<{ menu: MenuData[]; path: any }> = ({ menu, path }) => {
+export const HeaderNavigation: FC<{ menu: MenuData[]; path: any; handleSignOut: any; router: any }> = ({
+  menu,
+  path,
+  handleSignOut,
+  router,
+}) => {
   const { data: userRes, isLoading: userIsLoading } = useSWR("/users/me");
   const userInfo = !userIsLoading ? assignType("/users/me", userRes) : undefined;
+  const { user, isLoading } = useAuthState();
 
   const commonItemStyle = css({
     display: "block",
@@ -18,7 +26,7 @@ export const HeaderNavigation: FC<{ menu: MenuData[]; path: any }> = ({ menu, pa
   });
   return (
     <ul
-      className={hstack({
+      className={`${style_HeaderNavigation.headerNav} ${hstack({
         "& > *": {
           flexGrow: 1,
         },
@@ -27,17 +35,50 @@ export const HeaderNavigation: FC<{ menu: MenuData[]; path: any }> = ({ menu, pa
           base: "5px",
           lg: "0",
         },
+        marginX: {
+          base: "10px",
+          lg: "0",
+        },
+        overflowX: "auto",
         lg: { display: "none" },
-      })}>
+      })}`}>
       {(userInfo?.owned_project_id || path.startsWith("/committee")) &&
         menu.map((menu) => (
           <li key={menu.path}>
             <Link href={menu.path} className={commonItemStyle}>
               {menu.name}
-              {/* (HeaderNavigation) */}
             </Link>
           </li>
         ))}
+      {isLoading ? (
+        <></>
+      ) : user ? (
+        <li>
+          <button
+            onClick={handleSignOut}
+            className={`${style_HeaderNavigation.signoutBtn} ${css({
+              cursor: "pointer",
+              fontSize: "sm",
+              px: 5,
+              height: "100%",
+            })}`}>
+            ログアウト
+          </button>
+        </li>
+      ) : (
+        <li>
+          <button
+            onClick={() => router.push("/register")}
+            className={`${style_HeaderNavigation.signinBtn} ${css({
+              cursor: "pointer",
+              fontSize: "sm",
+              px: 5,
+              height: "100%",
+            })}`}>
+            ログイン / 新規登録
+          </button>
+        </li>
+      )}
     </ul>
   );
 };
