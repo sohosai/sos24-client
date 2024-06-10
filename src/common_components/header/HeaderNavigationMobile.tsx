24ec84c0f38951788088b0d0f9e46aa0bd7313c3
naohanpen <github@social.naohanpen.jp>
2024-06-10 17:13:16 +0900

    fix: Update button styles and remove unused header styles/components

---
+++ src/common_components/header/HeaderNavigationMobile.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { css } from "@styled-system/css";
+import { FC } from "react";
+import Link from "next/link";
+import { MenuData } from "./Header";
+import { hstack } from "@styled-system/patterns";
+import useSWR from "swr";
+import { assignType } from "@/lib/openapi";
+
+export const HeaderNavigationMobile: FC<{ menu: MenuData[]; path: any }> = ({ menu, path }) => {
+  const { data: userRes, isLoading: userIsLoading } = useSWR("/users/me");
+  const userInfo = !userIsLoading ? assignType("/users/me", userRes) : undefined;
+
+  const commonItemStyle = css({
+    display: "block",
+    height: "100%",
+    lineHeight: "100%",
+  });
+  return (
+    <ul
+      className={`${css({
+        "& > li": {
+          whiteSpace: "nowrap",
+          "&:not(.signoutBtn):not(.signinBtn)": {
+            transition: "all 0.1s",
+            _hover: {
+              color: "#ed6d1f",
+            },
+          },
+        },
+      })} ${hstack({
+        "& > *": {
+          flexGrow: 1,
+        },
+        textAlign: "center",
+        marginTop: {
+          base: "20px",
+          lg: "0",
+        },
+        marginX: {
+          base: "10px",
+          lg: "0",
+        },
+        lg: { display: "none" },
+      })}`}>
+      {(userInfo?.owned_project_id || path.startsWith("/committee")) &&
+        menu.map((menu) => (
+          <li key={menu.path}>
+            <Link href={menu.path} className={commonItemStyle}>
+              {menu.name}
+            </Link>
+          </li>
+        ))}
+    </ul>
+  );
+};
