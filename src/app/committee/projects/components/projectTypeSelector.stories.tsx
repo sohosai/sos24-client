663c145f8ac50be89e3d0bc02d0416937177b3d1
mikio <uaru0815@gmail.com>
2025-03-19 01:00:25 +0900

    storybook fileを追加

---
+++ src/app/committee/projects/components/projectTypeSelector.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import ProjectTypeSelector from "./projectTypeSelector";
+
+const meta: Meta<typeof ProjectTypeSelector> = {
+  component: ProjectTypeSelector,
+  argTypes: {
+    onChange: { action: "clicked" },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProjectTypeSelector>;
+
+export const Default: Story = {
+  args: {
+    value: {
+      location: [],
+      food: [],
+      committee: [],
+      attributes: [],
+    },
+    onChange: action("clicked"),
+  },
+};
+
+export const Selected: Story = {
+  args: {
+    value: {
+      location: [],
+      food: [],
+      committee: ["委員会でない"],
+      attributes: [],
+    },
+    onChange: action("clicked"),
+  },
+};
