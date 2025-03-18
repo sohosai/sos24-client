0c07b5c87f725ed3fc738c96fc4df4e0463e47f4
naohanpen <github@social.naohanpen.jp>
2025-03-18 23:53:21 +0900

    fix: スキーマに合わせて実装

--- src/app/committee/news/[news_id]/edit/EditNewsForm.tsx
+++ src/app/committee/news/[news_id]/edit/EditNewsForm.tsx
@@ -86,6 +86,7 @@ export const EditNewsForm: FC<{
             categories: categories as components["schemas"]["ProjectCategory"][],
             attributes: [...projectAttributes] as components["schemas"]["ProjectAttribute"][],
             attachments: fileIds["attachments"] ?? [],
+            state: "draft", // News 担当者が実装
           },
         })
         .then(({ error }) => {
