fcc6b0c67ceb281ab60fa8247c58829584e4b554
mikio <uaru0815@gmail.com>
2025-03-19 23:03:29 +0900

    セレクタを追加

--- src/app/committee/projects/page.tsx
+++ src/app/committee/projects/page.tsx
@@ -17,6 +17,9 @@ import { buttonStyle } from "@/recipes/button";
 import toast from "react-hot-toast";
 import { handleExport } from "@/lib/export";
 import { useAuthState } from "@/lib/firebase";
+import ProjectTypeSelector, { ProjectType } from "./components/ProjectTypeSelector";
+
+
 const filterStyle = css({
   position: "relative",
   background: `url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24"><path fill="%23000000" d="M0.443719 0.53466C0.443719 0.432857 0.546107 0.331055 0.716756 0.331055L9.24918 0.331055C9.41983 0.331055 9.52222 0.432857 9.52222 0.53466C9.52222 0.560111 9.52222 0.611012 9.48809 0.636463L5.22188 5.5739C5.15362 5.65025 5.05123 5.6757 4.98297 5.6757C4.94884 5.6757 4.81232 5.6757 4.74406 5.5739L0.477848 0.636463C0.443718 0.611012 0.443719 0.560111 0.443719 0.53466Z" /></svg>') no-repeat`,
@@ -42,6 +45,14 @@ const ProjectsPage: NextPage = () => {
       .sort((big, small) => big.index - small.index);
   })();
 
+
+    const [projectType, setProjectType] = useState<ProjectType>({
+      location: [],
+      food: [],
+      committee: [],
+      attributes: [],
+    });
+
   return (
     <div className={container({ marginY: 8 })}>
       <div className={hstack()}>
@@ -55,26 +66,8 @@ const ProjectsPage: NextPage = () => {
       </div>
       <div className={hstack({ justifyContent: "space-between", alignItems: "center", marginTop: 10 })}>
         <div className={hstack({ gap: 4 })}>
-          <select
-            onChange={(e) => setAttributesFilter(e.target.value as "" | components["schemas"]["ProjectAttribute"])}
-            className={cx(filterSelectorStyle, filterStyle)}>
-            <option value="">企画属性</option>
-            {projectAttributes.map((e) => (
-              <option value={e} key={e}>
-                <AttributesFormatter attribute={e as components["schemas"]["ProjectAttribute"]} />
-              </option>
-            ))}
-          </select>
-          <select
-            onChange={(e) => setCategoryFilter(e.target.value as "" | components["schemas"]["ProjectCategory"])}
-            className={cx(filterSelectorStyle, filterStyle)}>
-            <option value="">企画区分</option>
-            {projectCategories.map((e) => (
-              <option value={e} key={e}>
-                <ProjectCategoryFormatter category={e} />
-              </option>
-            ))}
-          </select>
+          <div>企画番号</div>
+          <div>場所番号</div>
         </div>
         <button
           className={buttonStyle({ visual: "outline", color: "purple" })}
@@ -96,15 +89,26 @@ const ProjectsPage: NextPage = () => {
           CSVダウンロード
         </button>
       </div>
-      {isLoading ? (
-        "Loading"
-      ) : error ? (
-        "error"
-      ) : generatedProjectData.length === 0 ? (
-        <NoResultNotice message="企画はありません" />
-      ) : (
-        <ProjectsList projectList={generatedProjectData} />
-      )}
+      <div className={css({
+        display: "flex",
+        gap: "40px", 
+        alignItems: "start",
+      })}>
+        <div className={css({minWidth: "200px", maxWidth: "300px", flexShrink: 0, paddingRight: "40px", borderRight: "1px solid", borderColor: "gray.300", marginTop: "20px",})}>
+          <ProjectTypeSelector value={projectType} onChange={setProjectType} />
+        </div>
+        <div className={css({flexGrow: 1, minWidth: "500px"})}>
+          {isLoading ? (
+            "Loading"
+          ) : error ? (
+            "error"
+          ) : generatedProjectData.length === 0 ? (
+            <NoResultNotice message="企画はありません" />
+          ) : (
+            <ProjectsList projectList={generatedProjectData} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
