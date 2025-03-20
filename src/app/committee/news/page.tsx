6e61ffa054c27ada44842dcb6cc49df63d0a19ce
an <170093728+nishimotoac@users.noreply.github.com>
2025-03-20 13:34:09 +0900

    お知らせ一覧のそれぞれのページを作成

--- src/app/committee/news/page.tsx
+++ src/app/committee/news/page.tsx
@@ -1,21 +1,18 @@
 "use client";
 
 import { NextPage } from "next";
-import { useState } from "react";
 import { center, container, stack } from "@styled-system/patterns";
 import { css } from "@styled-system/css";
 import { NewsView } from "@/common_components/news/NewsView";
-import { stateType } from "@/common_components/news/FilterNewsList";
 
 const CommitteeNewsPage: NextPage = () => {
-  const [choosenState, setChoosenState] = useState<stateType>("all"); //クリックした時にset~発動　filterの引数になる
   return (
     <div className={container()}>
       <div className={stack({ gap: 8, marginY: 8 })}>
         <h2 className={css({ fontSize: "2xl", fontWeight: "bold" })}>お知らせ一覧</h2>
         <div className={center()}>
           <div className={css({ width: "90%" })}>
-            <NewsView isCommittee filterByState={choosenState} />
+            <NewsView isCommittee filterByState={"all"} />
           </div>
         </div>
       </div>
