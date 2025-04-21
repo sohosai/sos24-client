f92c8b4f676fb3e8afe48d363c2a08db8fd170a8
naohanpen <github@social.naohanpen.jp>
2025-04-21 13:40:58 +0900

    fix: 筑波大学学園祭の応募方法とアカウント作成方法に関する情報を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,36 @@ export default function Home() {
           <div>
             <p>筑波大学学園祭「雙峰祭」の企画を募集いたします。</p>
             <p>企画に応募される方は、募集要項をご確認の上、企画募集期間中にご応募ください。</p>
+          </div>
+          <div>
             <p>募集要項は<a href="https://sohosai.com/news/#application-2025" target="_blank">公式ウェブサイトのお知らせ</a>をご覧ください。</p>
+          </div>
+          <div>
+            <p>応募方法</p>
+            <p>
+              <Link
+                href="/register"
+                className={css({
+                  color: "tsukuba.purple",
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                })}>
+                こちら
+              </Link>
+              よりご応募いただけます。
+            </p>
+            <p>アカウント作成方法を以下のリンクにて動画でご案内しております。</p>
+            <p>
+              <Link
+                href="/how-to-use"
+                className={css({
+                  color: "tsukuba.purple",
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                })}>
+                使い方動画（参考）
+              </Link>
+            </p>
           </div>
           <div>
             <p>学園祭実行委員会一同、素晴らしい雙峰祭を皆様と築いていけることを心より楽しみにしております。</p>
